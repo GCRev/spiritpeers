b81e3171d41b6b9f00e5911d6453995aeee02c27
Graham <graham.held@gmail.com>
2020-11-22 14:44:04 -0800

    Initial commit of files

---
+++ processIcons.js
@@ -0,0 +1,79 @@
+const fs = require('fs')
+const {Transform} = require('stream')
+// remove all the inline style information from all elements in the icon files
+
+const inputPathName = './src/icons_raw/'
+const outputPathName = './src/icons_proc/'
+
+class StyleRemove extends Transform {
+  constructor() {
+    super()
+    this.buffer = []
+    this.index = 0
+  }
+  checkBuffer() {
+    const chars=['s', 't', 'y', 'l', 'e']
+    let equal = true
+    for (let c = 0; c < chars.length && equal; c++) {
+      equal = this.buffer[this.buffer.length - chars.length + c] === chars[c].charCodeAt()
+    }
+    return equal
+  }
+  _transform(chunk, enc, callback) {
+    if (chunk) {
+      for (let a = 0; a < chunk.length; a++) {
+        const carCode = chunk[a]
+        let car = String.fromCharCode(carCode)
+        if (this.checkBuffer()) {
+          this.passedStyle = true
+          // remove these characters from the buffer
+          this.buffer.splice(-5, 5)
+        }
+        if (!this.passedStyle) {
+          this.buffer.push(carCode)
+        } else if (!this.quoteCar) {
+          // check opening quote and store it for a
+          // match later
+          if (car === "'" || car === '"') {
+            this.quoteCar = car
+          }
+        } else if (this.quoteCar) {
+          // if quoteCar has been assigned then 
+          // only reset after a match has been found
+          if (car === this.quoteCar) {
+            this.quoteCar = undefined
+            this.passedStyle = false
+          }
+        }
+
+        this.index ++
+      }
+    }
+    if (!this.passedStyle) {
+      // write out the buffer and clear the internal one
+      callback(null, Buffer.from(this.buffer))
+      this.buffer = []
+    }
+  }
+}
+
+async function processFile(fileName) {
+  const readStream = fs.createReadStream(inputPathName + '/' + fileName)
+  const styleRemove = new StyleRemove()
+  const outputStream = fs.createWriteStream(outputPathName + '/' + fileName)
+
+  readStream.pipe(styleRemove).pipe(outputStream)
+}
+
+async function processDir() {
+  try {
+    const files = await fs.promises.readdir(inputPathName)
+    for (const file of files) {
+      processFile(file)
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+processDir()
---
+++ webserver.mjs
@@ -0,0 +1,25 @@
+import Express from 'express'
+import Parser from 'body-parser'
+import Path from 'path'
+import ExpressWs from 'express-ws'
+
+const app = Express()
+
+app.use(Parser.json())
+const wsI = ExpressWs(app).getWss()
+
+app.listen(process.env.PORT || 54040, () => { console.log(`started on ${process.env.PORT || 54040}`)})
+
+const dirname = Path.resolve('.')
+
+app.get('/', (req, res)=> {
+  res.sendFile(`${dirname}/web/index.html`)
+})
+
+app.get('/web/*', (request, response) => {
+  response.sendFile(`${dirname}/web/${request.params['0']}`)
+})
+
+app.get('/src/*', (request, response) => {
+  response.sendFile(`${dirname}/src/${request.params['0']}`)
+})
