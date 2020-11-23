1db746976af698af3de5937e636505f45d6c4da7
Shad Aziz <shadaziz95@yahoo.com>
2020-11-22 19:22:40 -0800

    remade project
    remade project to make electron work with react properly

---
+++ public/electron.js
@@ -0,0 +1,58 @@
+const {app, ipcMain, BrowserWindow} = require('electron')
+const path =  require('path')
+const isDev = require('electron-is-dev')
+
+// app.allowRendererProcessReuse = false
+app.commandLine.appendSwitch('enable-webgl2-compute-context')
+app.commandLine.appendSwitch('use-cmd-decoder', 'passthrough')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1550,
+    height: 750,
+    webPreferences: {
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      allowRunningInsecureContent: false
+    }
+  })
+
+  mainWindow.removeMenu()
+  // and load the index.html of the app.
+  // mainWindow.loadFile(path.join(__dirname, 'app.html'))
+  mainWindow.loadURL(isDev ? 'http://localhost:3000/' : `file://${path.join(__dirname, "../build/index.html")}`);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
+  const bounds = mainWindow.getBounds()
+  // bounds.x = -2342 // do not hardcode
+  mainWindow.setBounds(bounds)
+
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+ipcMain.handle('hot-reload', mainWindow => {
+  createWindow()
+})
--- src/app.js
+++ src/app.js
@@ -1,56 +1,25 @@
-const {app, ipcMain, BrowserWindow} = require('electron')
-const path =  require('path')
+import logo from './logo.svg';
+import './App.css';
 
-// app.allowRendererProcessReuse = false
-app.commandLine.appendSwitch('enable-webgl2-compute-context')
-app.commandLine.appendSwitch('use-cmd-decoder', 'passthrough')
-
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1550,
-    height: 750,
-    webPreferences: {
-      enableRemoteModule: true,
-      nodeIntegration: true,
-      allowRunningInsecureContent: false
-    }
-  })
-
-  mainWindow.removeMenu()
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'app.html'))
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-  const bounds = mainWindow.getBounds()
-  // bounds.x = -2342 // do not hardcode
-  mainWindow.setBounds(bounds)
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
-})
-
-ipcMain.handle('hot-reload', mainWindow => {
-  createWindow()
-})
+export default App;
