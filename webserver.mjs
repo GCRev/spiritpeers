051344c433888df7605bebe0c39f28ce8f35614b
Graham <graham.held@gmail.com>
2020-11-22 19:09:05 -0800

    add cache directory and auto-create cache from webserver

--- webserver.mjs
+++ webserver.mjs
@@ -2,6 +2,7 @@ import Express from 'express'
 import Parser from 'body-parser'
 import Path from 'path'
 import ExpressWs from 'express-ws'
+import {promises as fs} from 'fs'
 
 const app = Express()
 
@@ -11,6 +12,8 @@ const wsI = ExpressWs(app).getWss()
 app.listen(process.env.PORT || 54040, () => { console.log(`started on ${process.env.PORT || 54040}`)})
 
 const dirname = Path.resolve('.')
+const contact_requests_path = `${dirname}/cache/contact_requests`
+const cache = new Map()
 
 app.get('/', (req, res)=> {
   res.sendFile(`${dirname}/web/index.html`)
@@ -26,4 +29,23 @@ app.get('/src/*', (request, response) => {
 
 app.get('/*', (request, response) => {
   response.sendFile(`${dirname}/web/${request.params['0']}`)
+})
+
+// check for the necessary chache/contact_requests directory
+fs.access(contact_requests_path)
+  .then(() => {
+    // load the requests cache
+    
+  })
+  .catch(err => {
+    // check err.code for ENOENT. If it's something else then we're phucked
+    if (err.code === 'ENOENT') {
+      return fs.mkdir(contact_requests_path, {recursive: true})
+    }
+  })
+  .catch(() => {
+    console.log('Unable to create cache requests directory')
+  })
+
+app.post('/wishfor', (request, response) => {
 })
