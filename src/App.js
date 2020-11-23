1714f38698295786e0198af63d879ba4c0bc8b61
Graham <graham.held@gmail.com>
2020-11-22 20:58:09 -0800

    moves some resources files to /public and adds icon.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div id="main-container" class="flex-center">
       <svg viewBox="0 0 128 128" class="icon box logo">
-        <use href="./src/icons_proc/peers_logo.svg#peers_logo"></use>
+        <use href="./icons_proc/peers_logo.svg#peers_logo"></use>
       </svg>
       <div id="click-to-download">SPIRIT</div>
     </div>
