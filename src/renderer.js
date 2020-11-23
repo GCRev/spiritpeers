1f84271f02da703539f1755f9a4781cd318cf3f2
Graham <graham.held@gmail.com>
2020-11-22 18:28:52 -0800

    add reload keyhandler in the renderer.js file to hack "hot-reloading"

--- src/renderer.js
+++ src/renderer.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './../global/index.css';
 
+window.addEventListener('keydown', evt => {
+  if (evt.code === 'KeyR' && evt.ctrlKey) {
+    ipcRenderer.invoke('hot-reload')
+    window.close()
+  }
+})
+
 class MessagingPage extends React.Component {
   constructor(props) {
     super(props);
